ce70a5e237aaa01880fdf4d350b03aae8b07d237
Mathias Quintero <mathias.quintero@tum.de>
2016-12-26 14:07:37 -0600

    Handling queries in standard URL

---
+++ Sources/Sweeft/Extensions/URL.swift
@@ -0,0 +1,18 @@
+//
+//  URL.swift
+//  Pods
+//
+//  Created by Mathias Quintero on 12/26/16.
+//
+//
+
+import Foundation
+
+public extension URL {
+    
+    func appendingQuery(key: String, value: String) -> URL {
+        var string = self.absoluteString + (??query ? "&" : "?") + "\(key)=\(value)"
+        return URL(string: string)!
+    }
+    
+}
