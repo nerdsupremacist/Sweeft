0faf55a96cf5cb7840ea1b76ad12492f62eba8c2
Mathias Quintero <mathias.quintero@tum.de>
2016-12-26 21:20:49 -0600

    Adding Observable objects and bindings to them

--- Sources/Sweeft/Binding.swift
+++ Sources/Sweeft/Binding.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+public struct Binding<T: Observable, O> {
+    
+    var value: T
+    let mapping: (T) -> O
+    
+    mutating func apply(to handler: @escaping (O) -> ()) {
+        let mapping = self.mapping
+        value | mapping | handler
+        value.onChange(do: mapping >>> handler)
+    }
+    
+}
+
+public func **<T: Observable, O>(_ value: T?, _ mapping: @escaping (T) -> (O)) -> Binding<T, O>? {
+    guard let value = value else {
+        return nil
+    }
+    return Binding(value: value, mapping: mapping)
+}
+
+public func =><T: Observable, O>(_ binding: Binding<T, O>?, _ handler: @escaping (O) -> ()) {
+    var binding = binding
+    binding?.apply(to: handler)
+}
+
+public func >>><T: Observable>(_ value: T?, _ handler: @escaping (T) -> ()) {
+    value ** id => handler
+}
+
+public func >>><C: Collection where C.Iterator.Element: Observable>(_ items: C, _ handler: @escaping (C.Iterator.Element) -> ()) {
+    items => { $0 >>> handler }
+}
+
+public func >>><T: Observable>(_ items: [T], _ handlers: [(T) -> ()]) {
+    guard items.count == handlers.count else {
+        return
+    }
+    handlers => {
+        (items | $1) >>> $0
+    }
+}
--- Sources/Sweeft/Observable.swift
+++ Sources/Sweeft/Observable.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+public protocol Observable {
+    var listeners: [Listener] { get set }
+}
+
+public extension Observable {
+    
+    typealias ListeningHandler = (Self) -> ()
+    typealias Listener = (handler: ListeningHandler, queue: DispatchQueue)
+    
+    public mutating func onChange(do handler: @escaping (Self) -> (), completionQueue: DispatchQueue = .main) {
+        // Add listener somehow
+        listeners.append((handler, completionQueue))
+    }
+    
+    public func hasChanged() {
+        listeners => { (listener: Listener) in
+            listener.queue >>> {
+                 listener.handler(self)
+            }
+        }
+    }
+    
+}
