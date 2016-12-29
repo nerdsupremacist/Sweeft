ad8d3ebd5a9ebed22e947aba00d243197c1323c7
Mathias Quintero <mathias.quintero@tum.de>
2016-12-29 16:34:37 -0600

    Adding basic Auth functionality

---
+++ Sources/Sweeft/Networking/Auth.swift
@@ -0,0 +1,38 @@
+//
+//  Auth.swift
+//  Pods
+//
+//  Created by Mathias Quintero on 12/29/16.
+//
+//
+
+import Foundation
+
+public protocol Auth {
+    func apply(to request: inout URLRequest)
+}
+
+public struct NoAuth: Auth {
+    
+    static let standard = NoAuth()
+    
+    public func apply(to request: inout URLRequest) {
+        // Do Nothing
+    }
+    
+}
+
+public struct BasicAuth {
+    let username: String
+    let password: String
+}
+
+extension BasicAuth: Auth {
+    
+    public func apply(to request: inout URLRequest) {
+        let string = ("\(username):\(password)".base64Encoded).?
+        let auth = "Basic \(string)"
+        request.addValue("Authorization", forHTTPHeaderField: auth)
+    }
+    
+}
