bf76a5e22c4f479309315d03320845c245f754a4
Mathias Quintero <mathias.quintero@tum.de>
2017-01-01 23:54:35 -0600

    Adding Generic API for endpoint

--- Sources/Sweeft/Networking/APIEndpoint.swift
+++ Sources/Sweeft/Networking/APIEndpoint.swift
@@ -13,3 +13,27 @@ public protocol APIEndpoint {
     /// Raw value string for the key
     var rawValue: String { get }
 }
+
+extension APIEndpoint {
+    
+    /// For the lazy. Creates a simple API object for your reference
+    public static func api(with url: String, baseHeaders: [String:String] = [:], baseQueries: [String:String] = [:]) -> GenericAPI<Self> {
+        return GenericAPI(baseURL: url, baseHeaders: baseHeaders, baseQueries: baseQueries)
+    }
+    
+}
+
+public struct GenericAPI<E: APIEndpoint>: API {
+    
+    public typealias Endpoint = E
+    public let baseURL: String
+    public let baseHeaders: [String:String]
+    public let baseQueries: [String:String]
+    
+    init(baseURL: String, baseHeaders: [String:String] = [:], baseQueries: [String:String] = [:]) {
+        self.baseURL = baseURL
+        self.baseHeaders = baseHeaders
+        self.baseQueries = baseQueries
+    }
+    
+}
