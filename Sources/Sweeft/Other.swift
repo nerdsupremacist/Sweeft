6152e7734436380a2a300045adb8cd9a1fdf3fb9
Mathias Quintero <mathias.quintero@tum.de>
2017-01-04 14:09:01 -0600

    Adding some useful functions like describe and apply and especially mapFirst and mapLast

--- Sources/Sweeft/Other.swift
+++ Sources/Sweeft/Other.swift
@@ -60,6 +60,74 @@ public func middleArgument<T, V, Z>(_ argOne: T, _ argTwo: V, _ argThree: Z) -> 
 }
 
 /**
+ Will map any input into by adding more input to it
+ 
+ - Parameter argument: value you want to append to the input
+ 
+ - Returns: function that passes the input through with the extra argument at the end
+ */
+public func add<V, T>(trailing argument: T) -> (V) -> (V, T) {
+    return { ($0, argument) }
+}
+
+/**
+ Will map any input into by adding more input to it
+ 
+ - Parameter argument: value you want to append to the input
+ 
+ - Returns: function that passes the input through with the extra argument at the start
+ */
+public func add<V, T>(starting argument: T) -> (V) -> (T, V) {
+    return { (argument, $0) }
+}
+
+/**
+ Will map a partial part of the input and compile it again into an output
+ 
+ - Parameter partial: gives the part of the input that should be mapped
+ - Parameter map: Maps the part to another value
+ - Parameter cleanup: compiles the original value with the new output to a new value
+ 
+ - Returns: mapping function
+ */
+public func partialMap<V, T, O, R>(partial: @escaping (V) -> (T), map: @escaping (T) -> (O), cleanup: @escaping (V, O) -> R) -> (V) -> (R) {
+    return { $0 | partial >>> map >>> add(starting: $0) >>> cleanup }
+}
+
+/**
+ Will map the first argument and leave the rest intact
+ 
+ - Parameter map: Maps the part to another value
+ 
+ - Returns: mapping function
+ */
+public func mapFirst<V, T, R>(_ map: @escaping (V) -> (R)) -> (V, T) -> (R, T) {
+    return flipArguments >>> mapLast(map) >>> flipArguments
+}
+
+/**
+ Will map the last argument and leave the rest intact
+ 
+ - Parameter map: Maps the part to another value
+ 
+ - Returns: mapping function
+ */
+public func mapLast<V, T, R>(_ map: @escaping (T) -> (R)) -> (V, T) -> (V, R) {
+    return partialMap(partial: lastArgument, map: map) { ($0.0, $1)}
+}
+
+/**
+ Will create a function that will get a function and apply the value to it
+ 
+ - Parameter value: Value that should be applied to inputed function
+ 
+ - Returns: function that takes a function and returns the result
+ */
+public func apply<T, V>(value: V) -> ((V) -> (T)) -> T {
+    return { value | $0 }
+}
+
+/**
  Will drop any arguments given to it. Who knows? Might be useful.
  
  - Parameter value: value
@@ -78,10 +146,35 @@ public func flipArguments<T, V>(_ argOne: T, _ argTwo: V) -> (V, T) {
     return (argTwo, argOne)
 }
 
+/**
+ Will increase the input by one. Can also be written as (+) ** 1
+ 
+ - Parameter number: n
+ 
+ - Returns: n + 1
+ */
 public func inc(_ number: Int) -> Int {
     return number + 1
 }
 
+/**
+ Will deliver the negative of a number
+ 
+ - Parameter number: n
+ 
+ - Returns: n * (-1)
+ */
 public func negative(_ number: Int) -> Int {
     return -number
+}
+
+/**
+ Will return the description of any input
+ 
+ - Parameter input: String convertible item
+ 
+ - Returns: String representation
+ */
+public func describe<T: CustomStringConvertible>(of input: T) -> String {
+    return input.description
 }
