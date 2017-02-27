c8c2224fe3005398ce32a367660c3d0703f8fec0
Mathias Quintero <mathias.quintero@tum.de>
2017-02-27 12:47:22 +0100

    Fixing error with url querie serialization

--- Sources/Sweeft/Extensions/URL.swift
+++ Sources/Sweeft/Extensions/URL.swift
@@ -19,8 +19,8 @@ public extension URL {
      - Returns: URL with the query
      */
     func appendingQuery(key: String, value: String) -> URL {
-        let query = "\(key)=\(value)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed).?
-        let string = self.absoluteString + (??query ? "&" : "?") + query
+        let newQuery = "\(key)=\(value)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed).?
+        let string = self.absoluteString + (??query ? "&" : "?") + newQuery
         return URL(string: string)!
     }
     
