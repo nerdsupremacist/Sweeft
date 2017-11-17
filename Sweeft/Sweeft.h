f33bc042646a7aef9459dc274335c8cb4d128563
Mathias Quintero <me@quintero.io>
2017-11-17 23:05:24 +0100

    Adding Carthage Support

---
+++ Sweeft/Sweeft.h
@@ -0,0 +1,19 @@
+//
+//  Sweeft.h
+//  Sweeft
+//
+//  Created by Mathias Quintero on 11/17/17.
+//  Copyright © 2017 Mathias Quintero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sweeft.
+FOUNDATION_EXPORT double SweeftVersionNumber;
+
+//! Project version string for Sweeft.
+FOUNDATION_EXPORT const unsigned char SweeftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sweeft/PublicHeader.h>
+
+
